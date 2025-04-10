008ec53f9f9fb44c97d07d3aeef7039d2bb5d5c9
Henk <hb130502@outlook.com>
2025-04-10 09:32:46 +0200

    Script to create config with variable dataset, model, poison rate; save_dir option in main.py

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from train import *
 
 def main(args):
     # Load the config
-    config_filepath = os.path.join('configs', f'{args.attack}.json')
+    config_filepath = os.path.join(args.save_dir, f'config.json')
     with open(config_filepath, 'r') as f:
         config = json.load(f)
 
     # Create a directory for the model
-    model_dir = os.path.join(f'model_dir_{args.attack}')
+    model_dir = args.save_dir
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
 
@@ -39,6 +39,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Deep Feature Space Trojaning Attack')
     parser.add_argument('--gpu', default='0', help='gpu id')
     parser.add_argument('--attack', default='dfst', help='attack type')
+    parser.add_argument('--save_dir', default='./save', help='directory for model and data')
+
     args = parser.parse_args()
 
     main(args)
